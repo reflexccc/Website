2903752a340cc47e8964fd4ecf36aeac7bd09856
reflexccc <162376235+reflexccc@users.noreply.github.com>
2026-06-18 22:50:24 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -7,24 +7,41 @@ const body = document.querySelector("#body");
 const titel = document.querySelector("#titel");
 const videos = document.querySelectorAll(".video");
 const lang = document.querySelectorAll(".lang");
+const alert = document.querySelector("alert");
+const alert_text = document.getElementById("alert_text");
 
-body.addEventListener("scroll", id_check);
-/*videos.forEach((vid, i) => {
-    vid.addEventListener("scroll", () => pause(vid));
-})*/
+body.addEventListener("scroll", id_check());
 
 btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => hidden(i));
+    btn.addEventListener("click", () => hide_cards(i));
 });
 
 icons.forEach((icn, i) => {
-    icn.addEventListener("click", () => to_id(i))
-})
+    icn.addEventListener("click", () => to_id(i));
+});
 
-function hidden(index){
+function show_alert(contact){
+    time = new Date();
+
+    alert_text.textContent = copytext(contact);
+
+    alert.classList.remove("hidden");
+    alert.style.zIndex = "2";
+    
+    setTimeout(() => {
+        alert.classList.add("hidden");
+        alert.style.zIndex = "-1";
+    }, 10000);
+};
+
+function toggle_hidden(element){
+    element.classList.toggle("hidden");
+};
+
+function hide_cards(index){
     btns[index].classList.toggle("dark");
-    text[index].classList.toggle("hidden");
-    jahre[index].classList.toggle("hidden");
+    toggle_hidden(text[index]);
+    toggle_hidden(jahre[index]);
 };
 
 function id_check(){
@@ -56,4 +73,6 @@ function copytext(element){
     const text = element.getAttribute('title');
 
     navigator.clipboard.writeText(text);
+
+    return(text);
 };
